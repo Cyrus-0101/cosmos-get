721fde623b90628cbdd35819b41080add448f385
Toxic254 <cyrus_g@outlook.com>
2023-01-18 13:02:19 +0300

    Added express json middleware, and catch all  unhandled routes

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import descriptionRoutes from "./mvc/routes/descriptionRoutes.js";
+
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/v1/descriptions", descriptionRoutes);
+
+app.use("*", (req, res) => {
+  res.json({ message: "API Routes are /api/v1/descriptions" });
+});
 
 app.listen(process.env.PORT, () => {
   console.info(`Server is running on port ${process.env.PORT}`);
