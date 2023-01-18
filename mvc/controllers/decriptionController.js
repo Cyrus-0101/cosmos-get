5d7835d216c41d96ec4e784cd287dfa890112e1f
Toxic254 <cyrus_g@outlook.com>
2023-01-18 13:03:40 +0300

    Setup connection to Azure table storage, to fetch all entities

--- mvc/controllers/decriptionController.js
+++ mvc/controllers/decriptionController.js
@@ -1,3 +1,27 @@
 import asyncHandler from "express-async-handler";
+import { TableClient } from "@azure/data-tables";
 
-export const getDescription = asyncHandler(async (req, res) => {});
+// Setup connection to Azure Table Storage
+const serviceClient = TableClient.fromConnectionString(
+  process.env.AZURE_STORAGE_CONNECTION_STRING, // The connection string to the storage account
+  process.env.AZURE_STORAGE_TABLE_NAME // The name of the table to create/connect to
+);
+
+// @desc - Fetch the Descriptions in the Azure Table Storage service
+// @route - /api/v1/descriptions
+// @access - Public
+// @method - GET
+export const getAllDescriptions = asyncHandler(async (req, res) => {
+  // Fetch all descriptions from Azure Table Storage
+  const entitiesArray = [];
+
+  const entities = serviceClient.listEntities();
+
+  // Add each entity to the array
+  for await (const entity of entities) {
+    entitiesArray.push(entity);
+  }
+
+  // Return the array
+  return res.status(200).json(entitiesArray);
+});
